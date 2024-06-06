3fd1e3332b7a972e54e41cd0d10659c96c09ca1c
peppone-choi <peppone.choi@gmail.com>
2024-06-07 01:49:56 +0900

    redux persist로 로컬 스토리지에 저장

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import LoginSlice from './login';
-const store = configureStore({
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+const reducers = combineReducers({
   reducer: {
     login: LoginSlice.reducer,
   },
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['login'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
 });
 
 export default store;
