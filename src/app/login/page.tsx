b808156fd66c13cd25dec6eb4dd6c73b6ee9c9db
peppone-choi <peppone.choi@gmail.com>
2024-06-07 02:30:41 +0900

    코드 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,7 +11,7 @@ const LoginPage = () => {
       <div className="flex justify-center">
         <div className="w-11/12 xl:w-9/12 p-2 xl:p-8 flex justify-center">
           <div className="w-full flex justify-center items-center">
-            <div className="w-10/12 flex justify-center items-center rounded-2xl shadow-2xl min-h-96 space-x-20">
+            <div className="w-10/12 flex justify-between px-40 items-center rounded-2xl shadow-2xl min-h-96">
               <div>소셜 로그인</div>
               <div>일반 로그인 + 회원가입</div>
             </div>
